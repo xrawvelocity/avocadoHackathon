855f43f4043a60e24d6c26fb1e028fcec61fe224
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-03-06 15:54:06 -0500

    added more to celebrities component

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ class App extends Component {
 
       </Switch>
       
+      <Celebrities/>
+      
     </div>
   );
 }
--- src/components/Celebrities.jsx
+++ src/components/Celebrities.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class Celebrities extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="celebStyles">
+                <h1>The Stars Love 'Em</h1>
+                <img src="./Kimkardashianavocado.png" alt="Logo" className="tomato_man" style={{ width: '300px'}}/>
+                <img src="./mileyavocado.png" alt="Logo" className="tomato_man" style={{ width: '300px'}}/>
+                <img src="./oprahavocado.png" alt="Logo" className="tomato_man" style={{ width: '300px'}}/>
             </div>
         )
     }
 }
+
+
